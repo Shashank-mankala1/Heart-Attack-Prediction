1d503c1f772142817b6ab3961ba4dcae68a649b9
Shashank Mankala <57833493+Shashank-mankala1@users.noreply.github.com>
2024-11-30 17:30:45 -0500

    Update model_api.R

--- model_api.R
+++ model_api.R
@@ -7,5 +7,7 @@ load("/app/heart_model.RData")
 pr <- plumb("api_routes.R")  # Replace with your route file if needed
 
 # Run the API on port 8000
-pr$run(host = "0.0.0.0", port = 8000)
+port <- as.numeric(Sys.getenv("PORT", "8000"))  # Default to 8000 if not set
+pr$run(host = "0.0.0.0", port = port)
+
 
