cc1df0775bd04ea3b8429d279a876344bb54a18b
Shashank Mankala <57833493+Shashank-mankala1@users.noreply.github.com>
2024-11-30 17:26:46 -0500

    Update model_api.R

--- model_api.R
+++ model_api.R
@@ -1,1 +1,11 @@
+library(plumber)
+
+# Load your model
+load("/app/heart_model.RData")
+
+# Define the API
+pr <- plumb("api_routes.R")  # Replace with your route file if needed
+
+# Run the API on port 8000
+pr$run(host = "0.0.0.0", port = 8000)
 
